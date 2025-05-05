01de0b60f9efe2a1db3b9825bf8a0a0ef614c727
Rohini Garg <rohini2garg1@gmail.com>
2025-05-05 18:35:52 +0530

    init

---
+++ app/api/v1/signup/route.ts
@@ -0,0 +1,14 @@
+import { NextRequest, NextResponse } from "next/server";
+
+
+export async function POST(req: NextRequest) {
+  //req.body?
+
+  const data = await req.json();
+
+  console.log(data);
+
+  return NextResponse.json({
+    message: "you have been signed up"
+  })
+}
---
+++ app/signin/page.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+export default function signin() {
+  return <div className="w-screen h-screen flex justify-center items-center">
+    <div className="border p-2">
+        <input type="text" placeholder="username"></input>
+        <input type="password" placeholder="password"></input>
+
+        <button onClick={() => {
+
+        }}>
+          Sign in
+        </button>
+    </div>
+
+  </div>
+}
